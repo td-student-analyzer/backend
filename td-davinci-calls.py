190b6d36febbd4fe199243a2a26677ed24ddf7dd
danyal1234 <dmahmood@mail.uoguelph.ca>
2019-09-14 20:55:07 -0400

    Update td-davinci-calls.py
    updates

--- td-davinci-calls.py
+++ td-davinci-calls.py
@@ -2,21 +2,22 @@
 import requests
 import json
 
-apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiMmYwZDNmZDctMThmMS0zYjdkLWI4ZjEtYmJmNGQxMGNjN2UwIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiI2MzYxNjM4NS01YjViLTRmYWYtOWYyNy0zNGU0MjZhYmJkODcifQ.1I1b3Ic7QfbXWVz0Fo1UVDd2gBTMucRJFbKWhW6DYpE'
+apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiMmYwZDNmZDctMThmMS0zYjdkLWI4ZjEtYm' \
+         'JmNGQxMGNjN2UwIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiI2MzYxNjM4NS01YjViLTRmYWYtOWYyNy0zNGU0MjZhY' \
+         'mJkODcifQ.1I1b3Ic7QfbXWVz0Fo1UVDd2gBTMucRJFbKWhW6DYpE'
 
 class Customer:
-  def __init__(self, balance = 0, education = 0, transport = 0, bills = 0, entertainment = 0, food = 0, income = 0,
-               loans = 0, shopping = 0, other = 0, totalEducation = 0, totalTransport = 0, totalBills = 0,
-               totalEntertainment = 0, totalFood = 0, totalIncome = 0, totalLoans = 0, totalShopping = 0, totalOther = 0):
+  def __init__(self, total=0, balance=0, education=0, transport=0, bills=0, entertainment=0, food=0,
+                 shopping=0, other=0, totalEducation=0, totalTransport=0, totalBills=0, totalEntertainment=0,
+                 totalFood=0, totalShopping=0, totalOther=0):
     self.balance = balance
+    self.total = total
 
     self.education = education
     self.transport = transport
     self.bills = bills
     self.entertainment = entertainment
     self.food = food
-    self.income = income
-    self.loans = loans
     self.shopping = shopping
     self.other = other
 
@@ -25,8 +26,6 @@ class Customer:
     self.totalBills = totalBills
     self.totalEntertainment = totalEntertainment
     self.totalFood = totalFood
-    self.totalIncome = totalIncome
-    self.totalLoans = totalLoans
     self.totalShopping = totalShopping
     self.totalOther = totalOther
 
@@ -36,18 +35,17 @@ class RatioAverages:
         self.lowMid = lowMid
 
 class YoungAdultAverage():
-    def __init__(self, balance = 0, education = 0, transport = 0, bills = 0, entertainment = 0, food = 0, income = 0,
-               loans = 0, shopping = 0, other = 0, totalEducation = 0, totalTransport = 0, totalBills = 0,
-               totalEntertainment = 0, totalFood = 0, totalIncome = 0, totalLoans = 0, totalShopping = 0, totalOther = 0):
+    def __init__(self, total=0, balance=0, education=0, transport=0, bills=0, entertainment=0, food=0,
+                 shopping=0, other=0, totalEducation=0, totalTransport=0, totalBills=0, totalEntertainment=0,
+                 totalFood=0, totalShopping=0, totalOther=0):
         self.balance = balance
+        self.total = total
 
         self.education = education
         self.transport = transport
         self.bills = bills
         self.entertainment = entertainment
         self.food = food
-        self.income = income
-        self.loans = loans
         self.shopping = shopping
         self.other = other
 
@@ -56,30 +54,21 @@ class YoungAdultAverage():
         self.totalBills = totalBills
         self.totalEntertainment = totalEntertainment
         self.totalFood = totalFood
-        self.totalIncome = totalIncome
-        self.totalLoans = totalLoans
         self.totalShopping = totalShopping
         self.totalOther = totalOther
-
-    def __repr__(self):
-        return print(self.balance, self.totalEducation, self.totalTransport, self.totalBills, self.totalEntertainment, self.totalFood, self.totalIncome, self.totalLoans, self.totalShopping, self.totalOther)
-
-    def __str__(self):
-        return print(self.balance, self.totalEducation, self.totalTransport, self.totalBills, self.totalEntertainment, self.totalFood, self.totalIncome, self.totalLoans, self.totalShopping, self.totalOther)
 
 class StudentAverage():
-    def __init__(self, balance = 0, education = 0, transport = 0, bills = 0, entertainment = 0, food = 0, income = 0,
-               loans = 0, shopping = 0, other = 0, totalEducation = 0, totalTransport = 0, totalBills = 0,
-               totalEntertainment = 0, totalFood = 0, totalIncome = 0, totalLoans = 0, totalShopping = 0, totalOther = 0):
+    def __init__(self, total=0, balance=0, education=0, transport=0, bills=0, entertainment=0, food=0,
+                 shopping=0, other=0, totalEducation=0, totalTransport=0, totalBills=0, totalEntertainment=0,
+                 totalFood=0, totalShopping=0, totalOther=0):
         self.balance = balance
+        self.total = total
 
         self.education = education
         self.transport = transport
         self.bills = bills
         self.entertainment = entertainment
         self.food = food
-        self.income = income
-        self.loans = loans
         self.shopping = shopping
         self.other = other
 
@@ -88,27 +77,21 @@ class StudentAverage():
         self.totalBills = totalBills
         self.totalEntertainment = totalEntertainment
         self.totalFood = totalFood
-        self.totalIncome = totalIncome
-        self.totalLoans = totalLoans
         self.totalShopping = totalShopping
         self.totalOther = totalOther
-
-    def __repr__(self):
-        return print(self.balance, self.totalEducation, self.totalTransport, self.totalBills, self.totalEntertainment, self.totalFood, self.totalIncome, self.totalLoans, self.totalShopping, self.totalOther)
-
-    def __str__(self):
-        return print(self.balance, self.totalEducation, self.totalTransport, self.totalBills, self.totalEntertainment, self.totalFood, self.totalIncome, self.totalLoans, self.totalShopping, self.totalOther)
-
 
 youngAdultCustomers = []
 studentCustomers = []
+
+adultAverage = YoungAdultAverage()
+studentAverage = StudentAverage()
 
 def getCustomerAccountsBalance(custId):
     response = requests.get('https://api.td-davinci.com/api/simulants/' + custId + '/simulatedaccounts',
     headers = { 'Authorization': apiKey })
     accounts = json.loads(response.text).get('result').get('bankAccounts')
     credits = json.loads(response.text).get('result').get('creditCardAccounts')
-    balance = 0
+    balance=0
     for account in accounts:
         balance += account.get('balance')
     for credit in credits:
@@ -126,12 +109,9 @@ def getTransactionHistory(custId):
                             json = {'continuationToken': '', 'searchCriteria': [{'key': 'originationDateTime', 'operation': '>', 'value': '2019-09'}]})
     return response.text
 
-def getTransactionByTag(custId, tag):
-    response = requests.post('https://api.td-davinci.com/api/simulants/' + custId + '/simulatedtransactions/search',
-                             headers={'Authorization': apiKey},
-                             json={'continuationToken': '', 'searchCriteria': [
-                                   {'key': 'originationDateTime', 'operation': '>', 'value': '2019-09'},
-                                   {'key': 'tags', 'operation': '=', 'value': tag}]})
+def getCustomerById(custId):
+    response = requests.get('https://api.td-davinci.com/api/customers/'+ custId +'/transactions',
+                            headers = {'Authorization': apiKey})
     return response.text
 
 def rawDataCall(token = ''):
@@ -144,75 +124,129 @@ def rawDataCall(token = ''):
 def getAllCustomers():
     virtualCustomers = json.loads(rawDataCall())
     i = 0
-    while len(virtualCustomers)>0 and i < 2:
+    while len(virtualCustomers) > 0 and i < 5:
         for virtualCustomer in virtualCustomers.get('result').get('customers'):
-            if virtualCustomer.get('age') < 30 and virtualCustomer.get('age') > 23:
+            if 23 < virtualCustomer.get('age') < 30:
                 youngAdultCustomers.append(virtualCustomer)
-            elif virtualCustomer.get('age') <= 23 and virtualCustomer.get('age') > 17:
+            elif 17 < virtualCustomer.get('age') <= 23:
                 studentCustomers.append(virtualCustomer)
 
         virtualCustomers = json.loads(rawDataCall(virtualCustomers.get('result').get('continuationToken')))
-        i+=1
+        i += 1
 
-
-def parseCustomers(loggedInCustID):
+def initialiseModel():
     getAllCustomers()
 
-    print(len(youngAdultCustomers))
-    print(len(studentCustomers))
-
-    adultAverage = YoungAdultAverage();
-
     for adult in youngAdultCustomers:
-        print(adult)
         balance = getCustomerAccountsBalance(adult.get('id'))
+        adultAverage.balance += balance
+        adultAverage.total += balance
         transactions = json.loads(getTransactionHistory(adult.get('id')))
         for transaction in transactions.get('result'):
-            amount = transaction.get('currencyAmount')
-            type = transaction.get('categoryTags')[0]
-            if type == 'Food and Dining':
+            amount = abs(transaction.get('currencyAmount'))
+            adultAverage.total += amount
+            tagType = transaction.get('categoryTags')[0]
+            if tagType == 'Food and Dining':
                 adultAverage.totalFood += amount
-            elif type == 'Education':
+            elif tagType == 'Education':
                 adultAverage.totalEducation += amount
-            elif type == 'Auto and Transport':
+            elif tagType == 'Auto and Transport':
                 adultAverage.totalTransport += amount
-            elif type == 'Bills and Utilities':
+            elif tagType == 'Bills and Utilities':
                 adultAverage.totalBills += amount
-            elif type == 'Entertainment':
+            elif tagType == 'Entertainment':
                 adultAverage.totalEntertainment += amount
-            elif type == 'Shopping':
+            elif tagType == 'Shopping':
                 adultAverage.totalShopping += amount
-            else:
+            elif tagType != 'Transfer':
                 adultAverage.totalOther += amount
-        adultAverage.balance += balance
 
-    studentAverage = StudentAverage();
+    adultAverage.food = adultAverage.totalFood / adultAverage.total
+    adultAverage.education = adultAverage.totalEducation / adultAverage.total
+    adultAverage.transport = adultAverage.totalTransport / adultAverage.total
+    adultAverage.bills = adultAverage.totalBills / adultAverage.total
+    adultAverage.entertainment = adultAverage.entertainment / adultAverage.total
+    adultAverage.shopping = adultAverage.shopping / adultAverage.total
+    adultAverage.other = adultAverage.other / adultAverage.total
 
     for student in studentCustomers:
-        print(student)
         balance = getCustomerAccountsBalance(student.get('id'))
+        studentAverage.balance += balance
+        studentAverage.total += balance
         transactions = json.loads(getTransactionHistory(student.get('id')))
         for transaction in transactions.get('result'):
-            amount = transaction.get('currencyAmount')
-            type = transaction.get('categoryTags')[0]
-            if type == 'Food and Dining':
+            amount = abs(transaction.get('currencyAmount'))
+            studentAverage.total += amount
+            tagType = transaction.get('categoryTags')[0]
+            if tagType == 'Food and Dining':
                 studentAverage.totalFood += amount
-            elif type == 'Education':
+            elif tagType == 'Education':
                 studentAverage.totalEducation += amount
-            elif type == 'Auto and Transport':
+            elif tagType == 'Auto and Transport':
                 studentAverage.totalTransport += amount
-            elif type == 'Bills and Utilities':
+            elif tagType == 'Bills and Utilities':
                 studentAverage.totalBills += amount
-            elif type == 'Entertainment':
+            elif tagType == 'Entertainment':
                 studentAverage.totalEntertainment += amount
-            elif type == 'Shopping':
+            elif tagType == 'Shopping':
                 studentAverage.totalShopping += amount
-            else:
+            elif tagType != 'Transfer':
                 studentAverage.totalOther += amount
-        studentAverage.balance += balance
 
-    print(adultAverage, studentAverage)
+    studentAverage.food = studentAverage.totalFood / studentAverage.total
+    studentAverage.education = studentAverage.totalEducation / studentAverage.total
+    studentAverage.transport = studentAverage.totalTransport / studentAverage.total
+    studentAverage.bills = studentAverage.totalBills / studentAverage.total
+    studentAverage.entertainment = studentAverage.entertainment / studentAverage.total
+    studentAverage.shopping = studentAverage.shopping / studentAverage.total
+    studentAverage.other = studentAverage.other / studentAverage.total
+
+
+def parseCustomer(loggedInCustID):
+    currentCustomer = Customer()
+    currentCustomer.balance = getCustomerAccountsBalance(loggedInCustID)
+    currentCustomer.total = currentCustomer.balance
+    transactions = json.loads(getTransactionHistory(loggedInCustID))
+    for transaction in transactions.get('result'):
+        amount = abs(transaction.get('currencyAmount'))
+        currentCustomer.total += amount
+        tagType = transaction.get('categoryTags')[0]
+        if tagType == 'Food and Dining':
+            currentCustomer.totalFood += amount
+        elif tagType == 'Education':
+            currentCustomer.totalEducation += amount
+        elif tagType == 'Auto and Transport':
+            currentCustomer.totalTransport += amount
+        elif tagType == 'Bills and Utilities':
+            currentCustomer.totalBills += amount
+        elif tagType == 'Entertainment':
+            currentCustomer.totalEntertainment += amount
+        elif tagType == 'Shopping':
+            currentCustomer.totalShopping += amount
+        elif tagType != 'Transfer':
+            currentCustomer.totalOther += amount
+
+    currentCustomer.food = currentCustomer.totalFood / currentCustomer.total
+    currentCustomer.education = currentCustomer.totalEducation / currentCustomer.total
+    currentCustomer.transport = currentCustomer.totalTransport / currentCustomer.total
+    currentCustomer.bills = currentCustomer.totalBills / currentCustomer.total
+    currentCustomer.entertainment = currentCustomer.totalEntertainment / currentCustomer.total
+    currentCustomer.shopping = currentCustomer.totalShopping / currentCustomer.total
+    currentCustomer.other = currentCustomer.totalOther / currentCustomer.total
+
+    return {adultAverage: adultAverage, studentAverage: studentAverage, currentCustomer: currentCustomer}
+
 
 def transactionsByTags(custId, tag):
-    print(getTransactionByTag(custId, tag))
-    return getTransactionByTag(custId, tag)
+    transactions = json.loads(getTransactionHistory(custId))
+    transactionsToReturn =[]
+
+    for transaction in transactions.get('result'):
+        type = transaction.get('categoryTags')[0]
+        if type == tag:
+            transactionsToReturn.append(transaction)
+
+    return transactionsToReturn
+
+
+initialiseModel()
